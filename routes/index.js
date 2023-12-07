5d39047fa4304737807b2c316196e1deb85604d5
amitnandan <a.amitnandan@gmail.com>
2023-12-07 21:57:26 +0530

    updated files added passport

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
 const postModel = require("./post");
+const passport = require("passport");
+
+const localStrategy = require("passport-local");
+passport.authenticate(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -44,5 +48,48 @@ router.get("/createpost", async function (req, res, next) {
   res.send("done");
   res.send(createdpost);
 });
+
+router.get("/profile", function (req, res, next) {
+  res.send("profile");
+});
+
+router.post("/register", function (req, res) {
+  const userData = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    fullname: req.body.fullname,
+  });
+
+  userModel.register(userData, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+  }),
+  function (req, res) {}
+);
+
+router.get("/logout", function (req, res) {
+  req.logOut(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/");
+  }
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/newapp");
 
@@ -31,6 +32,8 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.plugin(plm);
 
 const User = mongoose.model("User", userSchema);
 
